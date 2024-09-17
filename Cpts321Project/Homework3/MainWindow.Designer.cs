549337700a36cffb9639c302a30c5f95e47a62f7
Nathan <nlaha@outlook.com>
2024-09-16 18:13:01 -0700

    TDD & Project Creation

---
+++ Cpts321Project/Homework3/MainWindow.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Homework3
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            generate50FibonacciNumbersToolStripMenuItem = new ToolStripMenuItem();
+            generate100FibonacciNumbersToolStripMenuItem = new ToolStripMenuItem();
+            textBox1 = new TextBox();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackColor = SystemColors.Control;
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, saveToolStripMenuItem, toolStripSeparator1, generate50FibonacciNumbersToolStripMenuItem, generate100FibonacciNumbersToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(248, 22);
+            openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(248, 22);
+            saveToolStripMenuItem.Text = "Save";
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(245, 6);
+            // 
+            // generate50FibonacciNumbersToolStripMenuItem
+            // 
+            generate50FibonacciNumbersToolStripMenuItem.Name = "generate50FibonacciNumbersToolStripMenuItem";
+            generate50FibonacciNumbersToolStripMenuItem.Size = new Size(248, 22);
+            generate50FibonacciNumbersToolStripMenuItem.Text = "Generate 50 Fibonacci Numbers";
+            // 
+            // generate100FibonacciNumbersToolStripMenuItem
+            // 
+            generate100FibonacciNumbersToolStripMenuItem.Name = "generate100FibonacciNumbersToolStripMenuItem";
+            generate100FibonacciNumbersToolStripMenuItem.Size = new Size(248, 22);
+            generate100FibonacciNumbersToolStripMenuItem.Text = "Generate 100 Fibonacci Numbers";
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(0, 27);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(800, 424);
+            textBox1.TabIndex = 1;
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "MainWindow";
+            Text = "Notepad--";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem generate50FibonacciNumbersToolStripMenuItem;
+        private ToolStripMenuItem generate100FibonacciNumbersToolStripMenuItem;
+        private TextBox textBox1;
+    }
+}
---
+++ Cpts321Project/Homework3Tests/TestFibonacciReader.cs
@@ -0,0 +1,46 @@
+namespace Homework3Tests
+{
+    public class TestFibonacciReader
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void FibonacciReader_ReturnsFirst50Numbers()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Test]
+        public void FibonacciReader_ReturnsFirst100Numbers()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Test]
+        public void FibonacciReader_ThrowsOnNegativeInput()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Test]
+        public void FibonacciReader_ReturnsEmptyOnZeroInput()
+        {
+
+        }
+    }
+}
