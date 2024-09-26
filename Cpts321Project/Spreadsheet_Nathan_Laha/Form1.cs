75ace9655ebb87089d76bff33c8931dd61eb16a3
Nathan <nlaha@outlook.com>
2024-09-25 17:41:15 -0700

    Initial project creation and stylecop setup

---
+++ Cpts321Project/Spreadsheet_Nathan_Laha/Form1.cs
@@ -0,0 +1,10 @@
+namespace Spreadsheet_Nathan_Laha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Cpts321Project/Spreadsheet_Nathan_Laha/Program.cs
@@ -0,0 +1,24 @@
+// <copyright file="Program.cs" company="Nathan Laha">
+// 11762135
+// </copyright>
+
+namespace Spreadsheet_Nathan_Laha
+{
+    /// <summary>
+    /// Program class
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
