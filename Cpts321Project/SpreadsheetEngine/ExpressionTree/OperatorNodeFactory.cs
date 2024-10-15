d6d8b27f0f6ee63a000dfd9c22460645d49f2d22
Nathan <nlaha@outlook.com>
2024-10-15 10:01:45 -0700

    Refactor operator nodes to use factory and more inheritance

---
+++ Cpts321Project/SpreadsheetEngine/ExpressionTree/OperatorNodeFactory.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="OperatorNodeFactory.cs" company="Nathan Laha">
+// 11762135
+// </copyright>
+
+namespace SpreadsheetEngine.ExpressionTree
+{
+    using SpreadsheetEngine.ExpressionTree.Operators;
+
+    /// <summary>
+    /// Constructs operators
+    /// </summary>
+    internal class OperatorNodeFactory
+    {
+        /// <summary>
+        /// Creates a binary operator node based on the operator character
+        /// </summary>
+        /// <param name="op">the operator character, i.e. +, -</param>
+        /// <returns>the binary operator node</returns>
+        public static NodeBinaryOperator? CreateBinaryOperator(char op)
+        {
+            switch (op)
+            {
+                case '+':
+                    return new AdditionOperatorNode();
+                case '-':
+                    return new SubtractionOperatorNode();
+                case '*':
+                    return new MultiplicationOperatorNode();
+                case '/':
+                    return new DivisionOperatorNode();
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ Cpts321Project/SpreadsheetEngine/ExpressionTree/Operators/MultiplicationOperatorNode.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="MultiplicationOperatorNode.cs" company="Nathan Laha">
+// 11762135
+// </copyright>
+
+namespace SpreadsheetEngine.ExpressionTree.Operators
+{
+    /// <summary>
+    /// Multiplication Operator (*)
+    /// </summary>
+    internal class MultiplicationOperatorNode : NodeBinaryOperator
+    {
+        /// <inheritdoc/>
+        public override EAssociativity Associativity => EAssociativity.Left;
+
+        /// <inheritdoc/>
+        public override int Precedence => 0;
+
+        /// <inheritdoc/>
+        internal override double Evaluate()
+        {
+            if (this.Left == null || this.Right == null)
+            {
+                throw new InvalidOperationException("Binary operator has one or more null operands");
+            }
+
+            return this.Left.Evaluate() * this.Right.Evaluate();
+        }
+    }
+}
